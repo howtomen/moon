a2b1e67fd3a929e3462c69fedd792d056fc3d763
GreyWolfkin <57689976+GreyWolfkin@users.noreply.github.com>
2020-03-18 17:16:49 -0700

    Item Removal Functionality

---
+++ src/test/java/TestRemoveItem.java
@@ -0,0 +1,21 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestRemoveItem extends TestSuperClass {
+
+	@Test
+	public void canRemoveItemFromCart() throws CartException {
+		
+		boolean actual = new RemoveItemObject(driver)
+				.goToPage()
+				.addItemsToCart()
+				.confirmItemIsInCart()
+				.removeItemFromCart()
+				.confirmItemIsRemoved();
+
+		boolean expected = true;
+
+		Assert.assertEquals(actual, expected, "Blouse still in cart");
+	}
+
+}
